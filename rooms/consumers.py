a6e0248c652f26a5cb47071d44db12a125a475f2
David Odile <davidodile01@gmail.com>
2026-08-16 21:01:14 +0100

    Add chat_message() function to recieve events and turn them to websocket frames

--- rooms/consumers.py
+++ rooms/consumers.py
@@ -6,15 +6,26 @@ class RoomConsumer(AsyncWebsocketConsumer):
         
         room_name = self.scope['url_route']['kwargs']['room_name']
 
-        group_name = f'room_{room_name}'
+        self.group_name = f'room_{room_name}'
 
-        await self.channel_layer.group_add(group_name, self.channel_name)
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
 
         await self.accept()
 
 
     async def receive(self, text_data):
-        pass
 
-    def disconnect(self, close_code):
-        pass        
+        message = "hello bob"
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'chat.message',
+                "message": message
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(event["message"])
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
